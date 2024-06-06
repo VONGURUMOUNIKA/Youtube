222891cc69135784036bb8c3a0cfe6de80796461
VONGURUMOUNIKA <vongurumounika52@gmail.com>
2024-06-06 00:05:39 -0500

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import youtube from './Assets/download.png';
+import logo from './Assets/logo.png';
+
+
+const Navbar = () => {
+
+ return (
+    
+    <nav>
+        <div className="nav-center">
+          <div className="nav-header"> 
+            <img src={logo} alt='youtube' />
+          </div> 
+        </div>
+    </nav>
+
+ )
+
+
+}
+
+
+
+
+export default Navbar;
